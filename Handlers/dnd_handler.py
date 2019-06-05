e590faf9a857e1247e9c7f10252c33ebab90e7f1
sirdude103 <sirdude103@gmail.com>
2019-06-04 22:05:51 -0500

    Vastly improved the roll command

--- Handlers/dnd_handler.py
+++ Handlers/dnd_handler.py
@@ -12,17 +12,58 @@ class DNDHandler(BaseHandler):
 
     @classmethod
     async def roll(cls, message):
-        if "roll" in message.content:
-            position = message.content.index("roll")
+        if "roll" in message.content.lower():
+            position = message.content.lower().index("roll")
             position += len("roll")
             new_message = message.content[position:]
             split_message = new_message.split()
-            for part in split_message:
-                if len(part) >= 2 and part[0] == "d" and part[1:].isdigit() and int(part[1:]) != 0:
-                    die = int(part[1:])
-                    break
-            else:
-                die = 20
 
-            answer = random.randint(1, die)
-            await message.channel.send("Your d" + str(die) + " roll is: " + str(answer))
+            has_one_rolled = False
+            my_rolls = dict()
+            for part in split_message:
+                # search part for a die roll
+                for letter in part:
+                    if letter == "d":
+                        index = part.index("d")
+                        # determine if letter is part of die command or regular word
+                        if len(part) > index+1 and part[index+1:].isdigit() and int(part[index+1:]) != 0:
+                            has_one_rolled = True
+                            die = int(part[index+1:])
+                            if part[:index].isdigit():
+                                total = int(part[:index])
+                            else:
+                                total = 1
+
+                            if die not in my_rolls:
+                                my_rolls[die] = total
+                            else:
+                                my_rolls[die] += total
+                            break
+
+            if not has_one_rolled:
+                answer = random.randint(1, 20)
+                await message.channel.send("Your d20 roll is: " + str(answer))
+            else:
+                if len(my_rolls) > 5:
+                    await message.channel.send("Woah, man. I can get in trouble for rolling that much.")
+                    await message.channel.send("Try something shorter.")
+                else:
+                    for die, total in my_rolls.items():
+                        answer = 0
+                        int_rolls = list()
+                        my_message = "Your " + str(total) + "d" + str(die)
+                        if total == 1:
+                            my_message += " roll is:"
+                        else:
+                            my_message += " rolls are:"
+                        for _ in range(total):
+                            int_rolls.append(random.randint(1, die))
+                            my_message += " " + str(int_rolls[_])
+                            if _ != total-1:
+                                my_message += " +"
+                        my_message += " = " + str(sum(int_rolls))
+
+                        if len(my_message) >= 2000:
+                            my_message = my_message[:40] + " ... " + my_message[-40:]
+                        await message.channel.send(my_message)
+
